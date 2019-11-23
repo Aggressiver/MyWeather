fbbdee94c49ac268bd5638dc18e1fcdb89a726c1
wangkun <wangkun@tvmining.com>
2019-11-23 12:18:22 +0800

    testactivity

---
+++ app/src/main/java/com/csu/wangkun/myweather/activity/TestActivity.java
@@ -0,0 +1,17 @@
+package com.csu.wangkun.myweather.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @author wk
+ * @date 2019-11-23
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
